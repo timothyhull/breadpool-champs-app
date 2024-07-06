57f994d3756cd95a49e1f46813fd959910e19998
Timothy Hull <timothyhull@gmail.com>
2024-07-06 09:02:21 +0000

    Update for shell linting compliance

--- scripts/server-launch.sh
+++ scripts/server-launch.sh
@@ -3,7 +3,5 @@
 # Start the MkDocs service
 ## .devcontainer/devcontainer.json runs this file as a post attach command
 ## --config-file specifies the Material for MkDocs Insiders config file
-## --watch mkdocs.yml and --watch ./includes watches for changes in extra files
-mkdocs serve --config-file mkdocs.insiders.yml \
-    --watch mkdocs.yml \
-    # --watch ./includes
+## --watch watches for changes in extra files for automatic page reloads
+mkdocs serve --config-file mkdocs.insiders.yml --watch mkdocs.yml # --watch ./includes
